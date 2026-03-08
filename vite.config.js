a2ed06e347a880a76905bc6aff7dc336405a829b
dubaiexpat <ciaranlondra@gmail.com>
2026-03-08 20:22:01 +0800

    SSG: clean vite config, ssgOptions for vite-ssg CLI

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { ssgPlugin } from 'vite-plugin-ssg/react';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    ssgPlugin({
-      mock: true,
-      format: 'esm',
-      includeAllRoutes: true,
-    }),
-  ],
+  plugins: [react()],
+  ssgOptions: {
+    script: 'async',
+    formatting: 'minify',
+  },
 });
