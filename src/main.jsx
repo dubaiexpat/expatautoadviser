a6e03e549279399d600bc114f965b5675d53822d
dubaiexpat <ciaranlondra@gmail.com>
2026-03-08 20:09:58 +0800

    SSG: update entry point to use ViteSSG

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,5 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
+import { ViteSSG } from 'vite-plugin-ssg/react';
+import App from './App';
+import './index.css';
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+export const createApp = ViteSSG(App);
