d70390f2b5ed083112fa2111ccd2ddd057054e8c
dubaiexpat <ciaranlondra@gmail.com>
2026-03-08 20:10:19 +0800

    SSG: add vite-plugin-ssg to config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { ssgPlugin } from 'vite-plugin-ssg/react';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    ssgPlugin({
+      mock: true,
+      format: 'esm',
+      includeAllRoutes: true,
+    }),
+  ],
 });
